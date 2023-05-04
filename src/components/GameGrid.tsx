6b7c530a397c7b9d0bb65226f6b6d7abdaa025b7
Vladimir <sviderskijvladimir@gmail.com>
2023-05-04 15:51:09 +0300

    Refactoring - remove duplicated interfaces

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,10 +1,11 @@
 import { SimpleGrid, Text } from "@chakra-ui/react";
 import { GameQuery } from "../App";
-import useGames, { Platform } from "../hooks/useGames";
+import { Platform } from "../hooks/usePlatforms";
 import { Genre } from "../hooks/useGenres";
 import GameCard from "./GameCard";
 import GameCardContainer from "./GameCardContainer";
 import GameCardSkeleton from "./GameCardSkeleton";
+import useGames from "../hooks/useGames";
 
 interface Props {
   gameQuery: GameQuery;
